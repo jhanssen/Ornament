8e8c362b6c16a4b6f1fb41537a789f7131fca3c1
Jan Erik Hanssen <jhanssen@gmail.com>
2011-03-10 01:47:45 -0800

    Unneeded

--- main.cpp
+++ main.cpp
@@ -10,24 +10,6 @@ int main(int argc, char** argv)
 {
     QApplication app(argc, argv);
 
-    /*
-    AudioDevice device;
-
-    QStringList deviceList = device.devices();
-    if (deviceList.isEmpty())
-        return 1;
-
-    device.setDevice(deviceList.at(0));
-
-
-    AudioPlayer player;
-
-    player.setAudioDevice(&device);
-    player.setFilename("/Users/jhanssen/mp3/Bonobo/Animal Magic/02 - Sleepy Seven.mp3");
-
-    player.play();
-    */
-
     qmlRegisterType<AudioDevice>("AudioDevice", 1, 0, "AudioDevice");
     qmlRegisterType<AudioPlayer>("AudioPlayer", 1, 0, "AudioPlayer");
     qmlRegisterType<MusicModel>("MusicModel", 1, 0, "MusicModel");
