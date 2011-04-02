b668fec6f494ce59da77e600a2fb4f0bb18173f1
Jan Erik Hanssen <jhanssen@gmail.com>
2011-04-01 20:59:15 -0700

    Licensing

--- updater/trackduration.cpp
+++ updater/trackduration.cpp
@@ -1,3 +1,21 @@
+/*
+    Ornament - A cross plaform audio player
+    Copyright (C) 2011  Jan Erik Hanssen
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 #include "trackduration.h"
 #include "codecs/mad/mad/include/mad.h"
 #include "codecs/mad/taglib/taglib/mpeg/id3v2/id3v2frame.h"
